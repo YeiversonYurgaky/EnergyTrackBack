0f53d44844d6dc712b6955438cacdf59901b9dae
KarolyneRG <karolyne.renteriag@unac.edu.co>
2023-10-19 10:49:15 -0500

    :tada: The collection for consumption was created.

---
+++ src/models/ListaConsumosModels.js
@@ -0,0 +1,14 @@
+//datos sensibles y no sensibles. Contraseña, direccion, documento:
+//Todos los datos sensibles van encriptados
+
+const mongoose = require ("mongoose");
+const schema = mongoose.Schema;
+
+const ListaConsumosSchema = schema(
+    {
+        consumo_energetico : Number,
+        numero_de_piso: Number,
+    }
+);
+
+module.exports = mongoose.model("consumos_collection", ListaConsumosSchema);
