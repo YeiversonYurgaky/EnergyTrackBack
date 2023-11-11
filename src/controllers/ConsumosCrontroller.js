a929bae1e03c62cd1fedfce9d1dcacd61d643ab0
yeiversonyj <yeivercrack@gmail.com>
2023-11-10 20:24:21 -0500

    :heavy_plus_sign: Add new condition to if statement

--- src/controllers/ConsumosCrontroller.js
+++ src/controllers/ConsumosCrontroller.js
@@ -1,41 +1,43 @@
-const ConsumoModel = require("../models/ListaConsumosModels")
-const {CreateConsumo , deleteConsumo, updateConsumo, FindAllConsumo }= require("../repository/ConsumosRepository")
+const ConsumoModel = require("../models/ListaConsumosModels");
+const {
+  CreateConsumo,
+  deleteConsumo,
+  updateConsumo,
+  FindAllConsumo,
+} = require("../repository/ConsumosRepository");
 
 async function create(req, res) {
-    const params = req.body;
-  
-    const consumo = new ConsumoModel();
-  
-    if (
-      !params.consumo_energetico ||
-      !params.numero_de_piso 
-    ) {
-      res.status(400).send({ message: "Todos los campos son requeridos" });
-      return;
-    }
+  const params = req.body;
 
-    if (isNaN(params.consumo_energetico) || isNaN(params.numero_de_piso)) {
-      res
-        .status(400)
-        .send({ message: "Los campos solo pueden contener números" });
-      return;
-    }
+  const consumo = new ConsumoModel();
 
-    consumo.fecha = params.fecha;
-    consumo.consumo_energetico = params.consumo_energetico
-    consumo.numero_de_piso = params.numero_de_piso
+  if (!params.consumo_energetico || !params.numero_de_piso || !params.fecha) {
+    res.status(400).send({ message: "Todos los campos son requeridos" });
+    return;
+  }
 
-    const response = await CreateConsumo(consumo)
-    res.status(response.status).send(response)
+  if (isNaN(params.consumo_energetico) || isNaN(params.numero_de_piso)) {
+    res
+      .status(400)
+      .send({ message: "Los campos solo pueden contener números" });
+    return;
+  }
+
+  consumo.fecha = params.fecha;
+  consumo.consumo_energetico = params.consumo_energetico;
+  consumo.numero_de_piso = params.numero_de_piso;
+
+  const response = await CreateConsumo(consumo);
+  res.status(response.status).send(response);
 }
 
-async function deleteConsumoData(req, res){
+async function deleteConsumoData(req, res) {
   const id = req.params["id"];
   const response = await deleteConsumo(id);
   res.status(response.status).send(response);
 }
 
-async function updateConsumoData(req, res){
+async function updateConsumoData(req, res) {
   const id = req.params["id"];
   const body = req.body;
 
@@ -47,17 +49,14 @@ async function updateConsumoData(req, res){
   res.status(response.status).send(response);
 }
 
-async function findAll(req, res){
+async function findAll(req, res) {
   const response = await FindAllConsumo();
   res.status(response.status).send(response);
 }
-  
+
 module.exports = {
-    create, 
-    deleteConsumoData, 
-    updateConsumoData, 
-    findAll,
+  create,
+  deleteConsumoData,
+  updateConsumoData,
+  findAll,
 };
-
-
-  
