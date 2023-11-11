8f8cf2f32df968b8ba9945d586b6c586214d73ad
yeiversonyj <yeivercrack@gmail.com>
2023-11-10 20:23:47 -0500

    :recycle: Refactor code for improved readability

--- src/models/ListaConsumosModels.js
+++ src/models/ListaConsumosModels.js
@@ -1,15 +1,13 @@
 //datos sensibles y no sensibles. Contraseña, direccion, documento:
 //Todos los datos sensibles van encriptados
 
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const ListaConsumosSchema = schema(
-    {
-        consumo_energetico : Number,
-        numero_de_piso: Number,
-        fecha: Date,
-    }
-);
+const ListaConsumosSchema = schema({
+  consumo_energetico: Number,
+  numero_de_piso: Number,
+  fecha: Date,
+});
 
 module.exports = mongoose.model("consumos_collection", ListaConsumosSchema);
